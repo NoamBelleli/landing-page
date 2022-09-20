fba53e45091aa44ea3dda35cde7e1026ddd92715
Noam Belleli <noamhoter@gmail.com>
2022-09-20 14:45:26 +0300

    added form in index + functions to create navbar in js

--- js/app.js
+++ js/app.js
@@ -22,6 +22,86 @@
  * Define Global Variables
  * 
 */
+function createAboutUs() {
+    let ul = document.getElementById("navbar_list");
+    let li = document.createElement("li");
+    // li.textContent = 'About Us';
+    li.setAttribute("id", "#element_1");
+    ul.appendChild(li);
+};
+  
+createAboutUs();
+
+function createAboutUsLink() {
+    let liAbout = document.getElementById("#element_1");
+    let a = document.createElement("a");
+    a.setAttribute("href", "#Section 1");
+    a.textContent = "About Us";
+    liAbout.appendChild(a);
+};
+
+createAboutUsLink();
+
+function createProjects() {
+    let ul = document.getElementById("navbar__list");
+    let li = document.createElement("li");
+    // li.textContent = 'Projects';
+    li.setAttribute("id", "#element_2");
+    ul.appendChild(li);
+};
+  
+createProjects();
+
+function createProjectsLink() {
+    let liProjects = document.getElementById("#element_2");
+    let a = document.createElement("a");
+    a.setAttribute("href", "#Section 2");
+    a.textContent = "Projects";
+    liProjects.appendChild(a);
+};
+
+createProjectsLink();
+
+function createGallery() {
+    let ul = document.getElementById("navbar__list");
+    let li = document.createElement("li");
+    // li.textContent = 'Projects';
+    li.setAttribute("id", "#element_3");
+    ul.appendChild(li);
+};
+  
+createGallery();
+
+function createGalleryLink() {
+    let liGallery = document.getElementById("#element_3");
+    let a = document.createElement("a");
+    a.setAttribute("href", "#Section 3");
+    a.textContent = "Projects";
+    liGallery.appendChild(a);
+};
+
+createGalleryLink();
+
+function createSubscribe() {
+    let ul = document.getElementById("navbar__list");
+    let li = document.createElement("li");
+    // li.textContent = 'Projects';
+    li.setAttribute("id", "#element_4");
+    ul.appendChild(li);
+};
+  
+createSubscribe();
+
+function createSubscribeLink() {
+    let liSubscribe = document.getElementById("#element_3");
+    let a = document.createElement("a");
+    a.setAttribute("href", "#Section 3");
+    a.textContent = "Projects";
+    liSubscribe.appendChild(a);
+};
+
+createSubscribeLink();
+
 
 
 /**
